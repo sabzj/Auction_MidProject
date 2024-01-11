bf2f3350bd123c96a4a95850162cff897db96b47
sabzj <zjsabbah@gmail.com>
2024-01-12 00:25:59 +0200

    change

---
+++ src/components/mainPageAuction.js
@@ -0,0 +1,75 @@
+export const mainPageArray = [
+  {
+    id: 1,
+    name: "Harvest Machine",
+    imageUrl: "https://images.app.goo.gl/2P8Lp4FryQFkKEbv6",
+    address: "123 Farm Street, Agriculture Town",
+    type: "Agriculture",
+  },
+  {
+    id: 2,
+    name: "Smartphone",
+    imageUrl: "https://images.app.goo.gl/sw8bKLNtcGsW4YFAA",
+    address: "456 Tech Street, Electronics City",
+    type: "Electronics",
+  },
+  {
+    id: 3,
+    name: "cooling simulator",
+    imageUrl: "https://images.app.goo.gl/j3c87f4nztfGt7TR8",
+    address: "789 Food Street, Culinary Valley",
+    type: "Food Industry",
+  },
+  {
+    id: 4,
+    name: "Wooden Furniture",
+    imageUrl:
+      "https://www.service.european-aerosols.com/fileadmin/images/content/Anwendungsbeispiele/DUPLICOLOR/DEKO/Holzschutzlasur/Holzschutzlasur-01.jpg",
+    address: "101 Wood Street, Timberland",
+    type: "Woodworking",
+  },
+  {
+    id: 5,
+    name: "Truck",
+    imageUrl: "https://images.app.goo.gl/KdBcvxH4wXPLGuSA6",
+    address: "111 Shipping Lane, Logistics Harbor",
+    type: "Transport and Logistics",
+  },
+  {
+    id: 6,
+    name: "LED TV",
+    imageUrl: "https://example.com/ledtv.jpg",
+    address: "222 Tech Street, Electronics City",
+    type: "Electronics",
+  },
+  {
+    id: 7,
+    name: "Bakery Oven",
+    imageUrl: "https://example.com/bakeryoven.jpg",
+    address: "333 Food Street, Culinary Valley",
+    type: "Food Industry",
+  },
+  {
+    id: 8,
+    name: "Wooden Desk",
+    imageUrl: "https://example.com/woodendesk.jpg",
+    address: "444 Wood Street, Timberland",
+    type: "Woodworking",
+  },
+  {
+    id: 9,
+    name: "Truck",
+    imageUrl: "https://example.com/truck.jpg",
+    address: "555 Shipping Lane, Logistics Harbor",
+    type: "Transport and Logistics",
+  },
+  {
+    id: 10,
+    name: "Rice Harvester",
+    imageUrl: "https://example.com/riceharvester.jpg",
+    address: "666 Farm Street, Agriculture Town",
+    type: "Agriculture",
+  },
+];
+
+console.log(productsArray);
